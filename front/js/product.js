de1899e21474c007973363848f723a078fbf63b4
Raphael Deborde <raphaeldeborde61@gmail.com>
2023-01-26 12:03:37 +0100

    Ajout des couleurs dans le menu défilant select

--- front/js/product.js
+++ front/js/product.js
@@ -8,30 +8,35 @@ console.log(idCanape)
 //fonction pour afficher le Produit dans le html
 function afficherProduit(canape) {
 
+  // récupérer l'image du produit et la placé dans le DOM sous le bon parent
 var imageCanape = document.createElement("img");
-console.log(imageCanape);
-
 const div = document.querySelector("article .item__img");
-console.log(div)
 div.appendChild(imageCanape);
-
 imageCanape.src = canape.imageUrl;
-console.log(imageCanape.src)
 imageCanape.alt = canape.altTxt;
-console.log(imageCanape.alt)
 
+//récupérer le nom et le placé dans la balise ayant l'id title
 var nomCanape = document.getElementById("title");
 nomCanape.innerText = canape.name;
 console.log(canape.name)
 
+//récupérer le prix et l'insérer dans la balise ayant l'id prix
 var prixCanape = document.getElementById("price");
 prixCanape.innerText = canape.price;
 
-
+//récupérer la description du canapé et l'introduire dans la balise dont l'id est description
 var descriptionCanape = document.getElementById("description");
 descriptionCanape.innerText = canape.description;
-}
 
+var colors = canape.colors;
+var select = document.getElementById("colors")
+for (const color of colors) {
+  const newOption = document.createElement("option");
+  newOption.setAttribute("value", color);
+  newOption.innerText = color;
+  select.appendChild(newOption);
+  }
+}
 // afficher le produit grâce à l'api qui renvoit les données du produit suivant l'id dans l'url
 function recupererProduit(str) {
 
