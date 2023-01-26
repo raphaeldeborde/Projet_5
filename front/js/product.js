23ebac6c67c2930b85871044ce59e826313b0ea5
Raphael Deborde <raphaeldeborde61@gmail.com>
2023-01-26 09:29:33 +0100

    affichage page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,41 +1,47 @@
-var str = window.location.href;
+//récupérer l'url du site pour obtenir l'id du produit à afficher ensuite
+const str = document.location.href;
+const url = new URL(str);
 
-function afficherProduit (canape) {
+const idCanape = url.searchParams.get('id');
+console.log(idCanape)
+
+//fonction pour afficher le Produit dans le html
+function afficherProduit(canape) {
 
 var imageCanape = document.createElement("img");
-let baliseDivItemImg = document.getElementByClass("item__img");
-baliseDivItemImg.appendChild(imageCanape);
-imageCanape.src = canape.imageUrl;
-imageCanape.alt = canape.altTxt;
+console.log(imageCanape)
 
-var nomCanape = document.getElementById(title);
-var contenuTitre = document.createTextNode(canape.name);
-nomCanape.appendChild(contenuTitre);
+let div = document.getElementByClass("item__img");
+div.appendChildimageCanape
+
+imageCanape.src = canape.imageUrl;
+console.log(imageCanape.src)
+imageCanape.alt = canape.altTxt;
+console.log(imageCanape.alt)
+
+var nomCanape = document.getElementById("title");
+nomCanape.innerText = canape.name;
+console.log(canape.name)
 
 var prixCanape = document.getElementById("price");
-var contenuPrix = document.createTextNode(canape.price);
-prixCanape.appendChild(contenuPrix);
+prixCanape.innerText = canape.price;
+
 
 var descriptionCanape = document.getElementById("description");
-var contenuDescription = document.createTextNode(canape.description);
-descriptionCanape.appendChild(contenuDescription);
+descriptionCanape.innerText = canape.description;
 }
 
+// afficher le produit grâce à l'api qui renvoit les données du produit suivant l'id dans l'url
 function recupererProduit(str) {
 
-  var url = new URL(str);
-  var idCanape = url.searchParams.get("name");
-  let newIdCanape = idCanape.replace(/%20€/g, '');
-
-
-  fetch(`http://localhost:3000/api/products/${newIdCanape}`)
+  fetch('http://localhost:3000/api/products/'+idCanape)
     .then(function(reponse) {
       if (reponse.ok) {
         return reponse.json();
       }
     })
     .then(function(valeur) {
-      
+      console.log(valeur)
       afficherProduit(valeur);
 
       }
